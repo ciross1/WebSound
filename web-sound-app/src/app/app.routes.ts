f0721e87dfead2d0dd1edba1f5bf176ac0d70a20
ciross1 <oricsantamarina@gmail.com>
2025-10-17 01:02:46 +0200

    header 1.1

--- web-sound-app/src/app/app.routes.ts
+++ web-sound-app/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home'; // Importante importar el componente desde su ruta
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home-page', component:Home}  // En este caso home-page sería el url
+];
